5c7a3ef84eb19acf492792870f6d02f4c06dc35b
Peter <peterbergeld@gmail.com>
2026-09-14 15:40:43 +0200

    03 maxseats == Finns det plats eller inte till studenten

--- Course.cs
+++ Course.cs
@@ -6,4 +6,20 @@ class Course
     
     public List<Student> Students { get; set; } = new List<Student>(); // List of students enrolled in the course
 
+    public void Enroll (Student student)
+    {
+        Students.Count // 
+        if (Students.Count< MaxSeats); // Checks if there are any seats for potentiall students
+        Students.Add(student);
+        Students.Contains(student); // Checks the student aint in the list
+
+        if (Students.Count < MaxSeats && !Students.Contains(student)) // If students count is less than the maxseats = det finns plats
+        // && if the student doesnt exist.
+        {
+            Students.Add(student);
+        }
+        else 
+        {
+            Console.WriteLine("Kursen är full");
+        }
 }
