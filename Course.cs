e29dcea3c1a4aa44af55cc98e867e7196b891ed6
Peter <peterbergeld@gmail.com>
2026-09-14 16:38:23 +0200

    06 the Course-class done. On to the object

--- Course.cs
+++ Course.cs
@@ -1,3 +1,8 @@
+using System.Security.Cryptography.X509Certificates;
+// Really need to go over this code again 16:26 09-14-26
+// Alot of error-handling and some reading and more understanding of the code needed.
+// Understand the fundamentals and what it does, but need to place the 
+//exercise and code beside eachother
 class Course
 {
     
@@ -27,4 +32,29 @@ class Course
             
              Console.WriteLine("Kursen är full"); // If the course is already full 
         }
-}
+
+        public void Remove(Student student) // Secures so it wont crash if the student doenst exist
+    {
+        if (Students.Contains(student)) 
+        {
+            Students.Remove(student);
+        }
+        public void RollCall() // We need to write all the members in the list
+        {
+            foreach (Student student in Students) 
+        {
+            System.Console.WriteLine(student.Name);
+        }
+
+        public override string ToString()
+    {
+        return Name;
+        return $"{Name} ({Students.Count}/{MaxSeats} platser)"; // Basics in the Course-class done
+    }// unreachable code detected, sure i return it already yaa
+        }
+
+
+       
+
+        }
+    }
